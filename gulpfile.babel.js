4154e276a46b91293aa8987ec5ccdf019a4160e0
elbert4net <elbert@4net.com>
2021-04-16 08:29:57 +0200

    added gulp, pug and scss initial files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,41 @@
 import gulp from 'gulp';
+import pug from 'gulp-pug-3';
+import server from 'gulp-webserver';
+import livereload from 'gulp-livereload';
+import sass from 'gulp-dart-sass';
 
-gulp.task('default', async() => {
-	console.log('Gulp is working!');
+const paths = {
+	dist: 'dist',
+	pug: 'src/**/*.pug',
+	scss: 'src/**/*.scss'
+};
+
+gulp.task('pug', () => {
+	return gulp.src(paths.pug)
+		.pipe(pug({}))
+		.pipe(gulp.dest(paths.dist))
+		.pipe(livereload());
+});
+
+gulp.task('scss', () => {
+	return gulp.src(paths.scss)
+		.pipe(sass({}))
+		.pipe(gulp.dest(paths.dist))
+		.pipe(livereload());
 })
+
+gulp.task('server', () => {
+	return gulp.src(paths.dist)
+		.pipe(server({
+			open: false,
+			port: 8080
+		}));
+});
+
+gulp.task('watch', () => {
+	livereload.listen();
+	gulp.watch(paths.pug, gulp.series('pug'));
+	gulp.watch(paths.scss, gulp.series('scss'));
+});
+
+gulp.task('default', gulp.series('server', 'watch'));
