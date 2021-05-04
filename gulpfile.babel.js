2e342da45d7bc3b2162e37a6a20bc298a96cc582
elbert4net <elbert@4net.com>
2021-05-04 10:02:48 +0200

    header, hero, desktop nav, mobile nav (wip)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,50 +1,106 @@
+import concat from 'gulp-concat';
+import del from 'del';
 import gulp from 'gulp';
-import pug from 'gulp-pug';
-import server from 'gulp-webserver';
+import imagemin from 'gulp-imagemin';
 import livereload from 'gulp-livereload';
+import pug from 'gulp-pug';
 import sass from 'gulp-dart-sass';
+import sassLint from 'gulp-sass-lint';
+import server from 'gulp-webserver';
+import sourcemaps from 'gulp-sourcemaps';
 
-const paths = {
-	dist: 'dist',
-	pug: {
-		watch: 'src/templates/**/*.pug',
-		src: 'src/templates/*.pug'
-	},
-	scss: 'src/**/*.scss'
+const config = {
+	serverPort: 8080,
+	liveReloadPort: 8081
 };
 
-gulp.task('pug', () => {
-	return gulp.src(paths.pug.src)
+const templatesData = {
+	liveReloadPort: config.liveReloadPort,
+	title: 'UWV Content Platform',
+	author: 'elbert4net',
+};
+
+const paths = {
+	dist: 'dist/',
+	templates: {
+		watch: 	'src/templates/**/*.pug',
+		src: 	'src/templates/*.pug',
+		dist: 	'dist/',
+	},
+	images: {
+		watch:	'src/assets/images/**/*.*',
+		src:	'src/assets/images/**/*.*',
+		dist:	'dist/assets/images/',
+	},
+	styles: {
+		watch: 	'src/assets/styles/**/*.scss',
+		src: 	'src/assets/styles/*.scss',
+		dist:	'dist/assets/styles/'
+	},
+	scripts: {
+		watch: 	'src/assets/scripts/**/*.js',
+		src: 	'src/assets/scripts/*.js',
+		dist:	'dist/assets/scripts/'
+	}
+};
+
+gulp.task('templates', () => {
+	return gulp.src(paths.templates.src)
 		.pipe(pug({
-			pretty: true,
-			data: {
-				title: 'Zero',
-				author: 'Gizmo'
-			}
+			doctype: 'HTML',
+			pretty: '\t',
+			data: templatesData
 		}))
-		.pipe(gulp.dest(paths.dist))
+		.pipe(gulp.dest(paths.templates.dist))
 		.pipe(livereload());
 });
 
-gulp.task('scss', () => {
-	return gulp.src(paths.scss)
-		.pipe(sass({}))
-		.pipe(gulp.dest(paths.dist))
+gulp.task('images', function () {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin({
+            interlaced: true
+        }))
+        .pipe(gulp.dest(paths.images.dist))
 		.pipe(livereload());
-})
+});
+
+gulp.task('styles', () => {
+	return gulp.src(paths.styles.src)
+		.pipe(sourcemaps.init())
+		.pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+		.pipe(sass({}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(paths.styles.dist))
+		.pipe(livereload());
+});
+
+gulp.task('scripts', function () {
+    return gulp.src(paths.scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.scripts.dist))
+        .pipe(livereload());
+});
 
 gulp.task('server', () => {
 	return gulp.src(paths.dist)
 		.pipe(server({
 			open: false,
-			port: 8080
+			port: config.serverPort
 		}));
 });
 
 gulp.task('watch', () => {
-	livereload.listen();
-	gulp.watch(paths.pug.watch, gulp.series('pug'));
-	gulp.watch(paths.scss, gulp.series('scss'));
+	livereload.listen({
+		port: config.liveReloadPort
+	});
+	gulp.watch(paths.templates.watch,	gulp.series('templates'));
+	gulp.watch(paths.images.watch, 		gulp.series('images'));
+	gulp.watch(paths.styles.watch,		gulp.series('styles'));
+	gulp.watch(paths.scripts.watch,		gulp.series('scripts'));
 });
 
 gulp.task('default', gulp.series('server', 'watch'));
